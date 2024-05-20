50bcf5fa3c45a7bd0f9da32776e93670a47fb9cf
Anas Ashraf Fawzy Naguib <94796967+AnasAfawzy@users.noreply.github.com>
2024-05-21 00:13:55 +0300

    new data

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,2 +1,2 @@
-<a href="{{ route('name', 'anas') }}" style="margin-top: 25px;margin-left:25px">Click Me!</a>
+<a href="{{ route('test.name', 'anas') }}" style="margin-top: 25px;margin-left:25px">Click Me!</a>
 <h6>Anas</h6>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ItemController;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,7 +20,12 @@ Route::get('/', function () {
 });
 
 
-Route::get('/anas', [TestController::class, 'anas']);
-Route::get('/print_name/{name?}', [TestController::class, 'print_name'])->name('name');
+// Route::get('/anas', [TestController::class, 'anas']);
+// Route::get('/print_name/{name?}', [TestController::class, 'print_name'])->name('name');
 // Route::view('/anas', 'welcome');
 // Route::redirect('/heare', 'https://www.google.com/');
+Route::controller(TestController::class)->name('test.')->group(function () {
+    Route::get('/anas', 'anas')->middleware('check_user');
+    Route::get('/print_name/{name?}', 'print_name')->name('name');
+});
+Route::resource('items', ItemController::class)->only('index');
